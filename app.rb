d8899cc0e4324430d45ae698eb5325615dd237de
Tejas Manohar <tejas-manohar@users.noreply.github.com>
2014-11-09 09:10:16 -0600

    Update app.rb

--- app.rb
+++ app.rb
@@ -9,13 +9,22 @@ configure :development do
   set :port, 3000
 end
 
+Twilio.configure do |config|
+  config.account_sid = 'AC881f1d62823ef5127e2b2a7a1afa30f1'
+  config.auth_token = 'd743a50ba1688eba1cdba47a4eb63e51'
+end
+
 get '/' do
   'lol'
 end
 
 get '/receive' do
-  'lololol'
-  @client = Cleverbot::Client.new
-  @client.write params[:Body]
-  puts resp
+  @Cclient = Cleverbot::Client.new
+  @Cclient.write params[:Body]
+  @client = Twilio::REST::Client.new
+  @client.messages.create(
+    from: '+12084231206',
+    to: params[:From],
+    body: @Cclient[:message]
+  )
 end
