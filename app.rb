331d57a06b6f24c26277180bb306928c5093789c
Tejas Manohar <me@tejasmanohar.com>
2014-11-09 03:46:42 +0000

    will come back to this after helping others

--- app.rb
+++ app.rb
@@ -1,14 +1,21 @@
 require 'cleverbot'
 require 'twilio-ruby'
-require 'pry' if Sinatra::Base.development?
 require 'bundler'
 
 Bundler.require
+
+configure :development do   
+  set :bind, '0.0.0.0' 
+  set :port, 3000
+end
 
 get '/' do
   'lol'
 end
 
 get '/receive' do
-  'lolol'
+  'lololol'
+  @client = Cleverbot::Client.new
+  @client.write params[:Body]
+  puts resp
 end
