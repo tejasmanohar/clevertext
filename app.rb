512c9b3ddbfe73c7b93483198bb2a9a2bf05dcfd
Tejas Manohar <tejas-manohar@users.noreply.github.com>
2014-11-09 18:34:58 -0600

    Update app.rb

--- app.rb
+++ app.rb
@@ -14,13 +14,15 @@ Twilio.configure do |config|
   config.auth_token = 'd743a50ba1688eba1cdba47a4eb63e51'
 end
 
+get '/' do
+end
+
 get '/receive' do
-  @Cclient = Cleverbot::Client.new
-  @Cclient.write params[:Body]
+  array = ["Hi there", "I live in France", "No, you", "You are a liar", "Nice work", "You look good today", "Indeed", "I'm bored", "I'm hungry", "Are you my friend?"]
   @client = Twilio::REST::Client.new
   @client.messages.create(
     from: '+12084231206',
     to: params[:From],
-    body: @Cclient[:message]
+    body: array[Random.rand(9)]
   )
 end
